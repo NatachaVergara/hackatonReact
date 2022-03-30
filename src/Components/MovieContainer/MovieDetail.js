f6fa6470ff243a811e92caf8d9c750fffdc41885
Natacha Vergara <ntchvergara@gmail.com>
2022-03-30 13:37:30 -0300

    le saque los className vacios que tenian los li de movie genres

--- src/Components/MovieContainer/MovieDetail.js
+++ src/Components/MovieContainer/MovieDetail.js
@@ -4,7 +4,7 @@ import BtnAddCntx from '../BtnAddCntx'
 const MovieDetail = ({ movie }) => {
    
   return (
-      <div className="card col-1 m-2 d-flex flex-row justify-content-center aligne-items-center mt-5" id={movie.id} style={{ width: "60rem" }}>
+      <div className="card col-1 m-2 d-flex flex-md-row justify-content-center aligne-items-center mt-5" id={movie.id} style={{ width: "60rem" }}>
          
           <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} className="card-img-top p-1" alt={movie.original_title} />
           <div className="card-body bg-danger">
@@ -13,12 +13,12 @@ const MovieDetail = ({ movie }) => {
               <p>Tagline: {movie.tagline}</p>
               <p className="card-date">Release date: {movie.release_date}</p>
               <a href={movie.homepage} target="_blank" rel="noopener noreferrer"className="btn btn-outline-warning m-2">Visit homepage</a>
-              {/* <div className='d-flex flex-column m-2'>
+              <div className='d-flex flex-column m-2'>
                   <div className='d-flex flex-column'>
                       <h5 >Gender</h5>    
-                  <ul className="list-group list-unstyled">
-                  {movie.genres.map(li => (
-                      <li className="" key={li.id} >{li.name}  </li>
+                  <ul className="list-group list-unstyled mb-2">
+                  {movie.genres.map(genre => (
+                      <li key={genre.id} >{genre.name}  </li>
                   ))}
                       </ul>
                   </div>
@@ -26,15 +26,15 @@ const MovieDetail = ({ movie }) => {
                   <div className='d-flex flex-column'>
                       <h5>Production companies</h5>  
                       {
-                  movie.production_companies.map(p => (
-                      <ul className="list-group list-unstyled" id={p.id}>
-                          <li className="">
-                              {p.name}
+                  movie.production_companies.map(pc => (
+                      <ul className="list-group list-unstyled mb-2" id={pc.id}>
+                          <li key={pc.id}>
+                              {pc.name}
                           </li>
                   </ul>
                   ))} </div>
-              </div> */}
-              <Link to='/' className='btn btn-success '>Volver a inicio</Link>
+              </div>
+              <Link to='/' className='btn btn-success me-5'>Volver a inicio</Link>
               <BtnAddCntx movie={movie}  />
           </div>
       </div>
