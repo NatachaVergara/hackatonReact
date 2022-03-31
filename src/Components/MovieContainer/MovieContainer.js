53ec3eef8a2e54f1f36e4fb59a784455a1d5c709
Natacha Vergara <ntchvergara@gmail.com>
2022-03-30 21:59:20 -0300

    quite el locaStorage y agregue un condicional con un spinner en movieDetail

--- src/Components/MovieContainer/MovieContainer.js
+++ src/Components/MovieContainer/MovieContainer.js
@@ -3,25 +3,27 @@ import React, { useEffect, useState } from 'react'
 import getApi from '../../Api/getApi'
 import { useParams } from 'react-router-dom'
 import MovieDetail from './MovieDetail'
+import Spinner from '../Spinner'
 
-const getLocalMovie = () => {
-  let movie = localStorage.getItem('localMovie')
+// const getLocalMovie = () => {
+//   let movie = localStorage.getItem('localMovie')
 
-  if (movie) {
-    return JSON.parse(localStorage.getItem('localMovie')
-    )
-  } else {
-    return []
-  }
-}
+//   if (movie) {
+//     return JSON.parse(localStorage.getItem('localMovie')
+//     )
+//   } else {
+//     return []
+//   }
+// }
 
 
 
 const MovieContainer = () => {
-  const [movie, setMovie] = useState(getLocalMovie())
+ // const [movie, setMovie] = useState(getLocalMovie())
+  const [movie, setMovie] = useState([])
 
   const { id } = useParams()
-  localStorage.setItem('localMovie', JSON.stringify(movie))
+  // localStorage.setItem('localMovie', JSON.stringify(movie))
 
 
   useEffect(() => {
@@ -32,9 +34,15 @@ const MovieContainer = () => {
 
 
   return (
-    <div className='container m-auto mb-5 d-flex justify-content-center align-items-center row'>
-      <MovieDetail key={id} movie={movie} />
-    </div>
+    <>
+      {!movie ? <Spinner /> :
+        <div className='container text-warning m-auto mb-5 d-flex justify-content-center align-items-center row'>
+        <MovieDetail key={id} movie={movie} />
+      </div>
+      }
+
+    </>
+
 
   )
 }
